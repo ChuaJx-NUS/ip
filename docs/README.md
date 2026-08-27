3d0dfa8ddd9e8443b4f6ae785b0b840b25913f4b
Chua Jx <e1527727@u.nus.edu>
2026-08-27 23:22:52 +0800

    Level-0

--- docs/README.md
+++ docs/README.md
@@ -1,4 +1,4 @@
-# Duke User Guide
+# BigBrother User Guide
 
 // Update the title above to match the actual product name
 
@@ -17,7 +17,14 @@ Example: `keyword (optional arguments)`
 // A description of the expected outcome goes here
 
 ```
-expected output
+______ _      ______           _   _               
+| ___ (_)     | ___ \         | | | |              
+| |_/ /_  __ _| |_/ /_ __ ___ | |_| |__   ___ _ __ 
+| ___ \ |/ _` | ___ \ '__/ _ \| __| '_ \ / _ \ '__|
+| |_/ / | (_| | |_/ / | | (_) | |_| | | |  __/ |   
+\____/|_|\__, \____/|_|  \___/ \__|_| |_|\___|_|   
+          __/ |                                    
+         |___/                                      
 ```
 
 ## Feature ABC
---
+++ src/main/java/BigBrother.java
@@ -0,0 +1,20 @@
+public class BigBrother {
+    public static void main(String[] args) {
+        String banner = "______ _      ______           _   _\n"
+                        + "| ___ (_)     | ___ \\         | | | |\n"
+                        + "| |_/ /_  __ _| |_/ /_ __ ___ | |_| |__   ___ _ __\n"
+                        + "| ___ \\ |/ _` | ___ \\ '__/ _ \\| __| '_ \\ / _ \\ '__|\n"
+                        + "| |_/ / | (_| | |_/ / | | (_) | |_| | | |  __/ |\n"
+                        + "\\____/|_|\\__, \\____/|_|  \\___/ \\__|_| |_|\\___|_|\n"
+                        + "          __/ |\n"
+                        + "         |___/\n";
+
+        System.out.println("____________________________________________________________");
+        System.out.println(banner);
+        System.out.println("Hello! I'm BigBrother.");
+        System.out.println("What can I do for you?");
+        System.out.println("____________________________________________________________");
+        System.out.println("Bye. Hope to see you again soon!");
+        System.out.println("____________________________________________________________");
+    }
+}
