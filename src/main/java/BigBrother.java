38495318186595c247f5700dd326369fe0173ac4
Chua Jx <e1527727@u.nus.edu>
2026-08-31 21:46:14 +0800

    Complete Level-1

--- src/main/java/BigBrother.java
+++ src/main/java/BigBrother.java
@@ -1,5 +1,11 @@
+import java.util.Scanner;
+
+/**
+ * The entry point for the BigBrother chatbot.
+ */
 public class BigBrother {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
         String banner = "______ _      ______           _   _\n"
                         + "| ___ (_)     | ___ \\         | | | |\n"
                         + "| |_/ /_  __ _| |_/ /_ __ ___ | |_| |__   ___ _ __\n"
@@ -14,7 +20,20 @@ public class BigBrother {
         System.out.println("Hello! I'm BigBrother.");
         System.out.println("What can I do for you?");
         System.out.println("____________________________________________________________");
-        System.out.println("Bye. Hope to see you again soon!");
-        System.out.println("____________________________________________________________");
+
+        //Waiting for user response
+        while (scanner.hasNextLine()) {
+            String command = scanner.nextLine();
+            System.out.println("____________________________________________________________");
+
+            if (command.equals("bye")) {
+                System.out.println("Bye. Hope to see you again soon!");
+                System.out.println("____________________________________________________________");
+                break;
+            }
+
+            System.out.println("     " + command);
+            System.out.println("____________________________________________________________");
+        }
     }
 }
